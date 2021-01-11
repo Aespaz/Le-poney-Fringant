3b0e0fba1ff914cf541180240322f47282ab842d
Aespaz <espazeaymeric@gmail.com>
2021-01-11 14:27:01 +0100

    création de méthode de connection

---
+++ Backend/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'poney@localhost');
+define('DB_PASSWORD', 'fringant');
+define('DB_NAME', 'LEPONEYFRINGANT');
---
+++ index.js
@@ -0,0 +1,12 @@
+let soumettre = document.getElementById('SoumettreButton')
+
+let app = new vue({
+    el:'#app',
+        data:{
+            pseudo:NULL,
+            email: NULL,
+            password:NULL
+
+        }
+})
+    
