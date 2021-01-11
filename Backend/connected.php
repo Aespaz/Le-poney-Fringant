0446e1897d3a86efe6fdd85a71119f7db339a955
Aespaz <espazeaymeric@gmail.com>
2021-01-11 17:30:37 +0100

    commencement formulaire de connexion

--- Backend/connected.php
+++ Backend/connected.php
@@ -1,8 +1,8 @@
 <?php 
 
     session_start(); 
-    if(!empty($_SESSION['utilisateur'])) {
-        echo json_encode(["connected" => true, 'login' => $_SESSION['utilisateur']]);
+    if(!empty($_SESSION['utilisateurs'])) {
+        echo json_encode(["connected" => true, 'login' => $_SESSION['utilisateurs']]);
     } else {
         echo json_encode(["connected" => false]);
     }
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let soumettre = document.getElementById('SoumettreButton')
-
 let app = new vue({
     el:'#app',
         data:{
@@ -7,6 +5,33 @@ let app = new vue({
             email: NULL,
             password:NULL
 
-        }
+        },
+        mounted: function () {
+            fetch('http://localhost:8080/backend/connected.php')
+                .then(response => response.json())
+                .then((data) => {this.connected = data.connected; this.pseudo = data.pseudo});
+          },
+          methode:{
+              submit(ev) {
+                  ev.preventDefault();
+
+                  let formParams = new URLSearchParams();
+                  formParams.append("pseudo", this.pseudo);
+                  formParams.append("email", this.email);
+                  formParams.append("password", this.password);
+
+                  const requestOptions = {
+                      method: "POST",
+                      headers: {
+                          'Content-Type': 'application/x-www-form-urlencoded'
+                      },
+                      body: formParams
+                  };
+
+                  fetch('http://localhost:7000/Backend/Auth.php', requestOptions)
+                    .then(response => response.json())
+                    .then(data => (this.connecter = data.connected));
+              }
+          }
 })
     
